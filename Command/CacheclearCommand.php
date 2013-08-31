32a800a51e606b06b93a33813f61dbd5e94dd3cc
juanber <juanber@MacBook-Air-de-juanber.local>
2013-08-31 15:49:44 +0200

    first commit

---
+++ Command/CacheclearCommand.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Fourcoders\Bundle\CacheclearBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputDefinition;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CacheclearCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        parent::configure();
+
+        $this->setName('iddeos:empty:cache');
+    }
+
+    /**
+     * Executes the current command.
+     *
+     * @param InputInterface $input  An InputInterface instance
+     * @param OutputInterface $output An OutputInterface instance
+     *
+     * @return integer 0 if everything went fine, or an error code
+     *
+     * @throws \LogicException When this abstract class is not implemented
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        exec('rm -rf app/cache/*', $retval);
+        echo "Empty Cache Directory OK\n";
+        exec('rm -rf app/logs/*', $retval);
+        echo "Empty Logs Directory OK\n";
+        exec('setfacl -R -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs', $retval);
+        echo "Change Permissions Cache Directory OK\n";
+        exec('setfacl -dR -m u:www-data:rwx -m u:`whoami`:rwx app/cache app/logs', $retval);
+        echo "Change Permissions Logs Directory OK\n";
+        echo "Empty OK\n";
+    }
+}
---
+++ FourcodersCacheclearBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Fourcoders\Bundle\CacheclearBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FourcodersCacheclearBundle extends Bundle
+{
+}
